e57a754f67cbab7956c93e1739ddbaa9573b8db5
Laurent Mirabito <mirabito@lyoamdaq01.in2p3.fr>
2014-04-09 19:15:22 +0200

    ooops

---
+++ MultiThreaded/include/TkComputer.h
@@ -0,0 +1,37 @@
+#ifndef TkComputer_H_
+#define TkComputer_H_
+
+typedef struct
+{
+  float RhoMin;
+  float RhoMax;
+  uint32_t NRho;
+  uint32_t NTheta;
+  uint32_t NStubLow;
+  uint32_t NLayerRow;
+  uint32_t NStubLowCandidate;
+  uint32_t NBins3GeV;
+  uint32_t NBins5GeV; 
+  uint32_t NBins15GeV;
+  uint32_t NBins30GeV;
+  uint32_t NBins100GeV;
+  uint32_t NStubHigh;
+  uint32_t NLayerHigh;
+  uint32_t NStubHighCandidate;
+  float NDelBarrel;
+  float NDelInter;
+  float NDelEndcap;
+  
+} HoughCut;
+
+
+
+class TkComputer
+{
+public:
+  virtual void DefaultCuts()=0;
+  virtual void Compute(uint32_t isel,uint32_t nstub,float* x,float* y,float* z,uint32_t* layer)=0;
+  virtual void ComputeOneShot(uint32_t isel,uint32_t nstub,float* x,float* y,float* z,uint32_t* layer)=0;
+  virtual void ComputeTracklet(uint32_t isel,uint32_t nstub,float* x,float* y,float* z,uint32_t* layer,int32_t* flay)=0;
+};
+#endif
